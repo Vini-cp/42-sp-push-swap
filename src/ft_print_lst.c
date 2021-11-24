728629acd21d79642d8079d12ba1240308ef88f8
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-23 21:40:32 -0300

    Update print lst function

--- src/ft_print_lst.c
+++ src/ft_print_lst.c
@@ -6,18 +6,32 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 22:55:56 by vcordeir          #+#    #+#             */
-/*   Updated: 2021/11/21 22:55:57 by vcordeir         ###   ########.fr       */
+/*   Updated: 2021/11/23 19:53:29 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	ft_print_lst(t_list *lst)
+void	ft_print_lst(t_list *lst_a, t_list *lst_b)
 {
-	while(lst)
+	int size_a;
+	int size_b;
+
+	size_a = ft_lstsize(lst_a);
+	size_b = ft_lstsize(lst_b);
+	while(size_a)
 	{
-		ft_printf("%d\n", lst->content);
-		lst = lst->next;
+		ft_printf("%d ", lst_a->content);
+		lst_a = lst_a->next;
+		if (size_b == size_a)
+		{
+			ft_printf("%d", lst_b->content);
+			lst_b = lst_b->next;
+			size_b--;
+		}
+		size_a--;
+		ft_printf("\n");
 	}
+	ft_printf("- -\na b\n");
 	ft_printf("--------------------------------------------------------------\n");
 }
