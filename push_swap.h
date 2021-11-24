0d34740cddfc527cc10003c705be83597e03f2ee
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-11-23 21:42:09 -0300

    Update Makefile and header

--- push_swap.h
+++ push_swap.h
@@ -6,9 +6,13 @@
 # define TRUE 1
 # define FALSE 0
 
-void	  ft_print_lst(t_list *lst);
 t_list    *ft_initiate_stack(int argc, char *argv[]);
+void	  ft_print_lst(t_list *lst_a, t_list *lst_b);
 int     ft_is_sorted(t_list *lst);
 void    ft_swap(t_list **lst);
+void    ft_push(t_list **lst_a, t_list **lst_b);
+void    ft_rotate(t_list **lst);
+void    ft_reverse_rotate(t_list **lst);
+void    ft_movements(t_list **a, t_list **b, char *mov);
 
 #endif
